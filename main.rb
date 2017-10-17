40aefad72e20cb46163a3defe0425b4bc8059901
Fang <fang-wen.shen@vanderbilt.edu>
2017-10-16 21:47:09 -0500

    enumerators

---
+++ main.rb
@@ -0,0 +1,69 @@
+flowers = ["Everybody clap", "Moonwalk", "Shopping Cart", "Robot", "Twist", "Cabbage Patch", "Raise the roof", "Sprinkler", "Running Man", "Worm", "Shake your Ruby"].each
+length = flowers.inspect.split(",").length
+puts flowers.next
+puts flowers2 = flowers.next
+puts flowers.next
+puts flowers2
+puts flowers.rewind.next
+2.times {flowers.next}
+puts robot = flowers.next
+puts flowers.next_values
+puts robot
+puts flowers.rewind.next
+flowers.each_with_index {|num, i|
+	if i.next >= length
+		puts "#{num}"
+	end
+}
+puts flowers.rewind.next
+6.times {flowers.next}
+puts sprinkler = flowers.next
+flowers.rewind
+6.times {flowers.next}
+puts flowers.next
+flowers.next
+puts flowers.next
+flowers.rewind
+5.times {flowers.next}
+puts cabbage = flowers.next
+3.times {flowers.next}
+puts worm= flowers.grep(/Worm/)
+puts flowers.rewind.next
+flowers.each_with_index {|num, i|
+	if i.next >= length
+		puts "#{num}"
+	end
+}
+puts flowers.rewind.next
+puts flowers.next
+puts worm
+puts robot
+puts sprinkler
+puts flowers.rewind.next
+flowers.each_with_index {|num, i|
+	if i.next >= length
+		puts "#{num}"
+	end
+}
+puts flowers.rewind.next
+flowers.next
+puts flowers.next
+5.times {flowers.next}
+puts flowers.next
+flowers.rewind
+6.times {flowers.next}
+puts flowers.next
+flowers.rewind
+4.times {flowers.next}
+puts flowers.next
+flowers.each_with_index {|num, i|
+	if i.next >= length
+		puts "#{num}"
+	end
+}
+puts flowers.rewind.next
+flowers.each_with_index {|num, i|
+	if i.next >= length
+		puts "#{num}"
+	end
+}
